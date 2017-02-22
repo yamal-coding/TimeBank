1701449a29d8ae7652062e4e5ead968a20ca3f4b
yamal <yamal.coding@gmail.com>
2017-02-22 17:12:07 +0100

    Controller Payment Protocol have been defined

--- PaymentProtocolDemo/src/timebank/control/Controller.java
+++ PaymentProtocolDemo/src/timebank/control/Controller.java
@@ -31,41 +31,32 @@ public class Controller {
 		core.handleNotification(ref);
 	}
 	
+	//generacion de los tres primeros ficheros parciales
 	public void debitorPaymentPhase1(String ref, String comment, int degreeOfStisfaction){
 		core.paymentProtocolDebtorPhase1(ref, comment, degreeOfStisfaction);
-		
 	}
-	
+	//carga y validacion de los tres primeros ficheros parciales
 	public void creditorPaymentPhase1(String notificationRef){
 		core.paymentProtocolCreditorPhase1(notificationRef);
 	}
-	
+	//generacion de los 6 segundos ficheros parciales
 	public void creditorPaymentPhase2(String notificationRef, String comment, int degreeOfSatisfaction){
 		core.paymentProtocolCreditorPhase2(notificationRef, comment, degreeOfSatisfaction);
 	}
-	
+	//carga y validacion de los 6 segundos ficheros parciales
 	public void debitorPaymentPhase2(String notificationRef){
 		core.paymentProtocolDebitorPhase2(notificationRef);
 	}
-	
+	//generacion de los 3 ficheros finales del debtor y generacion de los 3 ultimos ficheros parciales del creditor
 	public void debitorPaymentPhase3(String notificationRef){
 		core.paymentProtocolDebitorPhase3(notificationRef);
 	}
-	
-	/*
-	public void startPaymentProtocolPhase1(){
-		
+	//carga y validacion de los 3 ultimos ficheros parciales del creditor
+	public void creditorPaymentPhase3(String notificationRef){
+		core.paymentProtocolCreditorPhase3(notificationRef);
 	}
 	
-	public void startPaymentProtocolPhase2(){
-		
+	public void creditorPaymentPhase4(String notificationRef){
+		core.paymentProtocolCreditorPhase4(notificationRef);
 	}
-	
-	public void startPaymentProtocolPhase3(){
-		
-	}
-	
-	public void startPaymentProtocolPhase4(){
-		
-	}*/
 }
